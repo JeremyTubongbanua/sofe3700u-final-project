50d381139f7f4bc952790ddd87119c5785373b78
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-12 14:31:42 -0500

    feat: working put

--- api/PutRecruit.js
+++ api/PutRecruit.js
@@ -1,13 +1,21 @@
 
 function responsePutRecruit(db, req, res) {
-    query = 'INSERT INTO recruit (u_name, pass_hash, full_name, recruit_location, bio, picture, recruit_resume, recruit_status_id) VALUES (\"' + req.body['u_name'] + '\", \"' + req.body['pass_hash'] + '\", \"' + req.body['full_name'] + '\", \"' + req.body['recruit_location'] + '\", \"' + req.body['bio'] + '\", \"' + req.body['picture'] + '\", \"' + req.body['recruit_resume'] + '\", \"' + req.body['recruit_status_id'] + '\");';
+    query = 'SELECT MAX(id) FROM recruit;';
     db.query(query, (err, result) => {
-        if(err) {
-            res.status(400).send({'message': 'error', 'result': err});
+        if (err) {
+            res.status(400).send({ 'message': 'error', 'result': err });
         } else {
-            res.status(200).send({'message': 'success', 'result': result});
+            const id = result[0]['MAX(id)'] + 1;
+            query = 'INSERT INTO recruit (id, u_name, pass_hash, full_name, recruit_location, bio, picture, recruit_resume, recruit_status_id) VALUES (' + id + ', \"' + req.body['u_name'] + '\", \"' + req.body['pass_hash'] + '\", \"' + req.body['full_name'] + '\", \"' + req.body['recruit_location'] + '\", \"' + req.body['bio'] + '\", \"' + req.body['picture'] + '\", \"' + req.body['recruit_resume'] + '\", ' + req.body['recruit_status_id'] + ');';
+            db.query(query, (err, result) => {
+                if (err) {
+                    res.status(400).send({ 'message': 'error', 'result': err });
+                } else {
+                    res.status(200).send({ 'message': 'success', 'result': result });
+                }
+            });
         }
     });
 }
 
-module.exports = {responsePutRecruit};
+module.exports = { responsePutRecruit };
