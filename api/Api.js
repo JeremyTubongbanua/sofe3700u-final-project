1936adcf40fc093ca8fc892b94b974cc74714080
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-11 22:24:34 -0500

    feat: pog

--- api/Api.js
+++ api/Api.js
@@ -23,16 +23,6 @@ db.connect((err) => {
     }
 });
 
-query = 'SELECT '
-
-db.query(query, (err, result) => {
-    if(err) {
-        console.log(err);
-    } else {
-        console.log(result);
-    }
-});
-
 app.get('/recruit/id', (req, res) => {
     const url = req.url;
     const body = req.body;
@@ -44,7 +34,23 @@ app.get('/recruit/id', (req, res) => {
     } else {
         // id is a number
         data = {}
-        res.status(200).send({'message': 'success', 'data': data});
+        query = 'SELECT recruit.id, recruit.u_name, recruit.pass_hash, recruit.full_name, recruit.recruit_location, recruit.bio, recruit.picture, recruit.recruit_resume, recruit_status.recruit_status, profession.profession FROM recruit JOIN recruit_status ON recruit.recruit_status_id = recruit_status.id JOIN recruit_professions ON recruit.id = recruit_professions.recruit_id RIGHT JOIN profession ON recruit_professions.profession_id = profession.id WHERE recruit.id = ' + body['id'] + ';'
+        db.query(query, (err, result) => {
+            if(err) {
+                res.status(400).send({'message': 'error'});
+            } else {
+                console.log('==================\n\nQUERY:\n' + query + '\n\nRECV:\n\n' + JSON.stringify(result) + '\n\n\n===========================');
+                data = result[0];
+                data['professions'] = [];
+                for(var i = 0; i < result.length; i++) {
+                    if(data['profession'] != null) {
+                        data['professions'].push(result[i]['profession']);
+                    }
+                }
+                data['profession'] = undefined;
+                res.status(200).send({'message': 'success', 'data': data});
+            }
+        });
     }
 });
 
