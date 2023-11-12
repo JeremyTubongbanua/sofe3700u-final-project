26719913b0be643e48bde5fc7db49f9f7bc826ae
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-11 22:00:24 -0500

    feat: db pog

--- api/Api.js
+++ api/Api.js
@@ -1,11 +1,47 @@
 
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
+const mysql = require('mysql2');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    console.log('Received: GET /');
+const app = express();
+app.use(express.json());
+dotenv.config({path: './.env'});
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    port : process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 });
+
+db.connect((err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('Successfully connected to database: ' + process.env.DB_DATABASE + ' as ' + process.env.DB_USER + '@' + process.env.DB_HOST + ':' + process.env.DB_PORT);
+    }
+});
+
+
+app.use(express.json());
+
+app.get('/recruit/id', (req, res) => {
+    const url = req.url;
+    const body = req.body;
+    console.log('GET ' + url + '    ' + JSON.stringify(body));
+
+    if(body === undefined || body['id'] === undefined || body['id'] === NaN) {
+        // does not exist, or is not a number
+        res.status(400).send({'message': 'id is required and must be a number'});
+    } else {
+        // id is a number
+        data = {}
+        res.status(200).send({'message': 'success', 'data': data});
+    }
+});
+
+app.post('/recruit')
 
 const server = app.listen(3000, '0.0.0.0', () => {
     const host = server.address().address;
