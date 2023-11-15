436d69e5919a005dd3da9e23448315ef3cbc58d9
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2023-11-15 18:24:03 -0500

    Update profession.js

--- web/src/components/recruit/professions/profession.js
+++ web/src/components/recruit/professions/profession.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function Profession(props) {
 
     return (
         <div>
+            <Form>
+
+                {/* When doing this in future: props.id should replace the id and label = props.name type is just the checkbox being mapped over */}
+
+                {['checkbox'].map((type) => (
+                    <div key={`default-${type}`} className="mb-3">
+                        <Form.Check // prettier-ignore
+                            type={type}
+                            id={`default-${type}`}
+                            label={`default ${type}`}
+                        />
+                    </div>
+                ))}
+            </Form>
             {props.name}
         </div>
     );
