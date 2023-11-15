9206f078b983ae2984c59a051926d6b29f2ae6a3
Jerry Shum <30613366+JerryShum@users.noreply.github.com>
2023-11-15 14:46:27 -0500

    Finish Header

--- web/src/components/recruit/navBar.js
+++ web/src/components/recruit/navBar.js
@@ -6,23 +6,23 @@ import './navBar.css';
 
 function NavBar() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-            <Container>
-                <Navbar.Brand href="#home" className='logo'>Super Cool Jobs</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    {/* <Nav>
-                        <div className='d-flex flex-row justify-content-between align-items-center'>
-                            <Nav.Link href="#home" className="me-3">Job Postings</Nav.Link>
-                            <Nav.Link href="#link" className="me-3">My Profile</Nav.Link>
-                            <button type="submit" className="btn btn-dark w-75 p-3">
-                                Logout
-                            </button>
-                        </div>
-                    </Nav> */}
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+
+        <Nav className="d-flex justify-content-around align-items-center w-100 p-4 overflow-hidden" activeKey="/home">
+            <Navbar.Brand href="/home" className='logo'>Super Cool Jobs</Navbar.Brand>
+            <Nav.Item>
+                <Nav.Link href="/" className='link-text'>Job Postings</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link href="/" className='link-text'>Profile</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+                <Nav.Link>
+                    <button type="submit" className="btn btn-outline-dark w-80 p-3 px-5">
+                        Log Out
+                    </button>
+                </Nav.Link>
+            </Nav.Item>
+        </Nav>
     );
 }
 
