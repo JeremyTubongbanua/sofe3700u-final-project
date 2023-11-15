04a13560e48d1f277f8660fba208dbda70524d75
JerryShum <jerryshum2636@gmail.com>
2023-11-15 02:23:44 -0500

    Finished home page

--- web/src/index.js
+++ web/src/index.js
@@ -5,13 +5,14 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Card, Button, Form, CardBody, FormLabel, FormControl } from 'react-bootstrap'
 import SignUp from './components/signUp'
 import Login from './components/login';
+import Home from './components/home'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Router>
             <Routes>
-                <Route path="/home" Component={Home} />
+                <Route path="/" Component={Home} />
                 <Route path="/signup" Component={SignUp} />
                 <Route path="/login" Component={Login} />
             </Routes>
