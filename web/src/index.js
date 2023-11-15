d9ba6a055503f0c8197c5f40abe951a1d8deeecf
JerryShum <jerryshum2636@gmail.com>
2023-11-14 23:00:39 -0500

    signup page

--- web/src/index.js
+++ web/src/index.js
@@ -1,43 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Card, Button, Form, CardBody, FormLabel, FormControl } from 'react-bootstrap'
-
-function App() {
-    return (
-        <div>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-0">Sign Up</h2>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className='w-100 text-center mt-2'></div>
-        </div>
-    );
-}
+import SignUp from './components/signUp'
+import Header from './components/header';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <div>
+            <Header />
+            <SignUp />
+        </div>
     </React.StrictMode>
 )
