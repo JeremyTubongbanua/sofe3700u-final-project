afde6ab2ce5700282857eecd0b9951143ae4881d
JerryShum <jerryshum2636@gmail.com>
2023-11-20 03:02:24 -0500

    Fixing Navbar, implemented proper routing
    Still need to fix active css.

--- web/src/components/recruit/recruitNav/recruitNav.js
+++ web/src/components/recruit/recruitNav/recruitNav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './navBar.css';
 
@@ -10,9 +10,16 @@ function RecruitNav() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="d-flex justify-content-around align-items-center w-100 overflow-hidden">
-                    <Nav.Link as={Link} to="/recruit" className='logo'>Super Cool Jobs</Nav.Link>
-                    <Nav.Link as={Link} to="/recruit/profile">Recruit Profile</Nav.Link>
-                    <Nav.Link as={Link} to="/recruit/posting">Recruit Posting</Nav.Link>
+                    <NavLink to="/recruit" className='logo' activeClassName='active-link'>
+                        Super Cool Jobs
+                    </NavLink>
+
+                    <NavLink to="/recruit/profile" className='link-text' activeStyle={{ borderBottom: '2px solid gray' }}>
+                        Recruit Profile
+                    </NavLink>
+                    <NavLink to="/recruit/posting" className='link-text' activeStyle={{ borderBottom: '2px solid gray' }}>
+                        Recruit Posting
+                    </NavLink>
                     <Nav.Link>
                         <button type="submit" className="btn btn-outline-dark w-80 p-3 px-5">
                             Log Out
