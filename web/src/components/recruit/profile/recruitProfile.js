9726b085bcd21424f390c139755a1daac8053d8d
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-21 00:36:41 -0500

    feat: loading recruit, not done editable fetch

--- web/src/components/recruit/profile/recruitProfile.js
+++ web/src/components/recruit/profile/recruitProfile.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, Col, Form, Row, Image, Stack } from 'react-bootstrap';
 
 function RecruitProfile(props) {
+
     return (
         <div className='d-flex align-items-center justify-content-center' style={{ minHeight: '100vh' }}>
             <Form style={{ width: '100%', maxWidth: '800px' }}>
@@ -15,17 +16,21 @@ function RecruitProfile(props) {
                                 </h1>
 
                                 <div className='image-container' style={{ width: '200px', height: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
-                                    <Image className="h-100 w-100" src={props.picture} rounded fluid />
+                                    <Image className="h-100 w-100" src={"https://i.imgur.com/SbwZN0K.png"} rounded fluid />
                                 </div>
 
                                 <Form.Group controlId="formStatus">
                                     <Form.Label>Status</Form.Label>
-                                    <Form.Control className='rounded-0' placeholder={props.recruit_status} />
+                                    <Form.Control className='rounded-0' value={props.recruit_status} onChange={(e) => {
+                                        props.setRecruitStatus(e.target.value);
+                                    }} />
                                 </Form.Group>
 
                                 <Form.Group controlId="formProfession">
                                     <Form.Label>Professions</Form.Label>
-                                    <Form.Control className='rounded-0' placeholder={props.professions} />
+                                    <Form.Control className='rounded-0' value={props.professions} onChange={(e) => {
+                                        props.setProfessions(e.target.value);
+                                    }} />
                                 </Form.Group>
 
                                 <Form.Group>
@@ -40,27 +45,40 @@ function RecruitProfile(props) {
                             <Stack gap={3}>
                                 <Form.Group controlId="formGridUsername">
                                     <Form.Label>Full Name</Form.Label>
-                                    <Form.Control className='rounded-0' type="fullname" placeholder={props.full_name} />
+                                    <Form.Control className='rounded-0' type="fullname" value={props.full_name} onChange={(e) => {
+                                        props.setFullName(e.target.value);
+                                    }} />
                                 </Form.Group>
 
                                 <Form.Group controlId="formGridPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control className='rounded-0' type="password" placeholder={props.pass_hash} />
+                                    <Form.Control className='rounded-0' type="password" value={props.pass_hash} onChange={(e) => {
+                                        props.setPassHash(e.target.value);
+                                    }} />
                                 </Form.Group>
 
                                 <Form.Group controlId="formGridAddress1">
                                     <Form.Label>Address</Form.Label>
-                                    <Form.Control className='rounded-0' placeholder={props.recruit_location} />
+                                    <Form.Control className='rounded-0' value={props.recruit_location} onChange={(e) => {
+                                        props.setRecruitLocation(e.target.value);
+                                    }} />
                                 </Form.Group>
 
                                 <Form.Group controlId="formResume">
                                     <Form.Label>Resume URL</Form.Label>
-                                    <Form.Control className='rounded-0' placeholder={props.recruit_resume} />
+                                    <Form.Control className='rounded-0' value={props.recruit_resume} onChange={(e) => {
+                                        props.setRecruitResume(e.target.value);
+                                    }} />
                                 </Form.Group>
 
                                 <Form.Group controlId="formBio">
                                     <Form.Label>Bio</Form.Label>
-                                    <Form.Control className='rounded-0' style={{ resize: 'none' }} as="textarea" rows={6} placeholder={props.bio} />
+                                    <Form.Control className='rounded-0' style={{ resize: 'none' }} as="textarea" rows={6} value={props.bio}
+
+                                        onChange={(e) => {
+                                            props.setBio(e.target.value);
+                                        }}
+                                    />
                                 </Form.Group>
                             </Stack>
                         </Col>
--- web/src/components/recruit/recruitIndex.js
+++ web/src/components/recruit/recruitIndex.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RecruitNav from './recruitNav/recruitNav';
 import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
 import RecruitHome from './recruitHome/recruitHome';
@@ -6,13 +6,71 @@ import RecruitProfile from './profile/recruitProfile';
 import PostingView from './postings/postingView';
 
 function RecruitIndex() {
+
+    const [id, setId] = useState('');
+    const [u_name, setUName] = useState('');
+    const [passHash, setPassHash] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [recruitLocation, setRecruitLocation] = useState('');
+    const [bio, setBio] = useState('');
+    const [picture, setPicture] = useState('');
+    const [recruitResume, setRecruitResume] = useState('');
+    const [recruitStatus, setRecruitStatus] = useState('');
+    const [professions, setProfessions] = useState('');
+
+    useEffect(() => {
+        setUName(document.cookie.split('; ').find(row => row.startsWith('u_name')).split('=')[1]);
+        const url = 'http://jeremymark.ca:3001/recruit/u_name';
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify({
+                u_name: u_name
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then((data) => {
+                data = data.data
+                setId(data.id);
+                setPassHash(data.pass_hash);
+                setFullName(data.full_name);
+                setRecruitLocation(data.recruit_location);
+                setBio(data.bio);
+                setPicture(data.picture);
+                setRecruitResume(data.recruit_resume);
+                setRecruitStatus(data.recruit_status);
+                setProfessions(data.professions);
+            }).catch(err => console.log(err));
+    }, [u_name]);
+
     return (
         <div className='position-relative'>
             <RecruitNav />
             <Routes>
                 <Route path="/" element={<Outlet />}>
                     <Route index element={<RecruitHome />} />
-                    <Route path="profile" element={<RecruitProfile />} />
+                    <Route path="profile" element={<RecruitProfile
+                        u_name={u_name}
+                        setUName={setUName}
+                        pass_hash={passHash}
+                        setPassHash={setPassHash}
+                        full_name={fullName}
+                        setFullName={setFullName}
+                        recruit_location={recruitLocation}
+                        setRecruitLocation={setRecruitLocation}
+                        bio={bio}
+                        setBio={setBio}
+                        picture={picture}
+                        setPicture={setPicture}
+                        recruit_resume={recruitResume}
+                        setRecruitResume={setRecruitResume}
+                        recruit_status={recruitStatus}
+                        setRecruitStatus={setRecruitStatus}
+                        professions={professions}
+                        setProfessions={setProfessions}
+                    />} />
                     <Route path="posting" element={<PostingView />} />
                 </Route>
             </Routes>
