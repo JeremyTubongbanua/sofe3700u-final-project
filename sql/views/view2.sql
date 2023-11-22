cf8a7bb83b05437bf71b7d468841cb392bfc9bd2
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-22 14:07:24 -0500

    view2

--- sql/views/view2.sql
+++ sql/views/view2.sql
@@ -5,5 +5,11 @@
 DROP VIEW IF EXISTS view2;
 
 CREATE VIEW view2 AS (
+    SELECT r.recruit_id, r.recruit_name, AVG(j.job_salary) AS average_salary
+    FROM recruits r
+    INNER JOIN applications a ON r.recruit_id = a.recruit_id
+    INNER JOIN jobs j ON a.job_id = j.job_id
+    GROUP BY r.recruit_id, r.recruit_name
+);
     
 );
