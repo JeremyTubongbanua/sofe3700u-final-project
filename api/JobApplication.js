f973d71807fe8ff232210feae0f51696214401bb
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-22 02:29:17 -0500

    123

--- api/JobApplication.js
+++ api/JobApplication.js
@@ -34,23 +34,13 @@ const responseGetJobApplicationsByRecruitId = (db, req, res) => {
         res.status(400).send({'message': 'error', error: 'id is required'});
     } else {
         const id = req.body['id'];
-        query = 'SELECT job_posting.id, job_posting.company_id, job_posting.job_posting_title, job_posting.job_posting_description, job_posting.salary, job_posting.picture, job_posting_status.job_posting_status, job_posting_type.job_posting_type, profession.profession FROM job_application INNER JOIN job_posting ON job_application.job_posting_id = job_posting.id INNER JOIN job_posting_status ON job_posting.job_posting_status_id = job_posting_status.id INNER JOIN job_posting_type ON job_posting.job_posting_type_id = job_posting_type.id INNER JOIN profession ON job_posting.profession_id = profession.id WHERE job_application.recruit_id = ' + id + ';';
+        query = 'SELECT job_posting.id, job_posting.company_id, job_posting.job_posting_title, job_posting.job_posting_description, job_posting.salary, job_posting.picture, job_posting_status.job_posting_status, job_posting_type.job_posting_type, profession.profession FROM job_application INNER JOIN job_posting ON job_application.job_posting_id = job_posting.id INNER JOIN job_posting_status ON job_posting.job_posting_status_id = job_posting_status.id INNER JOIN job_posting_type ON job_posting.job_posting_type_id = job_posting_type.id WHERE job_application.recruit_id = ' + id + ';';
         db.query(query, (err, result) => {
             if(err) {
                 console.log(err);
                 res.status(500).send({'message': 'error', 'error': err});
             } else {
                 data = result;
-                for (let i = 0; i < data.length; i++) {
-                    professions = [];
-                    for(let j = 0; j < data.length; j++) {
-                        if(data[i].id === data[j].id) {
-                            professions.push(data[j].profession);
-                        }
-                    }
-                    data[i].professions = professions;
-                    delete data[i].profession;
-                }
                 res.status(200).send({'message': 'success', 'data': data});
             }
         });
