30cd59835bf391a5f98fb1b8bebff414b65d6438
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2023-11-22 03:20:26 -0500

    feat: poggers

--- web/src/components/recruiter/companyPostings/companyPostings.js
+++ web/src/components/recruiter/companyPostings/companyPostings.js
@@ -1,46 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Container, Row, Col, Image } from 'react-bootstrap';
 import './companyPostings.css'
 
 function CompanyPostings() {
-    const postings = [
+    /**
+     * [
         {
-            id: 0,
-            title: 'job_posting_title',
-            salary: '$60,000 - $90,000',
-            status: 'open',
-            position: 'Software Engineer',
-            type: 'Hybrid',
-            picture: 'https://images.pexels.com/photos/7173026/pexels-photo-7173026.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' // Add a picture property to each posting
-        },
-        // More postings...
-    ];
+            "id": 2,
+            "company_id": 1,
+            "job_posting_title": "Project Manager",
+            "job_posting_description": "Someone to manage the google developers in their weekly scrum meetings and facilitate team activity and meetings.",
+            "salary": 50000,
+            "picture": "https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            "job_posting_status": "Open",
+            "job_posting_type": "Remote",
+            "job_posting_frequency": "Contract",
+            "professions": [
+                "Sales",
+                "Data Science",
+                "Data Analytics"
+            ]
+        }
+    ]
+     */
+    const [jobPostings, setJobPostings] = useState([]);
 
-    const applicants = [
-        {
-            name: 'Applicant',
-            bio: 'Bio',
-            professions: 'Software Engineer',
-            picture: 'https://images.pexels.com/photos/8951199/pexels-photo-8951199.jpeg?auto=compress&cs=tinysrgb&w=1600'
-        },
-        {
-            name: 'Applicant',
-            bio: 'Bio',
-            professions: 'Software Engineer',
-            picture: 'https://images.pexels.com/photos/8951199/pexels-photo-8951199.jpeg?auto=compress&cs=tinysrgb&w=1600'
-        },
-        {
-            name: 'Applicant',
-            bio: 'Bio',
-            professions: 'Software Engineer',
-            picture: 'https://images.pexels.com/photos/8951199/pexels-photo-8951199.jpeg?auto=compress&cs=tinysrgb&w=1600'
-        },
-        // More applicants...
-    ];
+    useEffect(() => {
+        const url = 'http://jeremymark.ca:3001/company/job_postings';
+        fetch(url, {
+            'method': 'POST',
+            'body': JSON.stringify({
+                id: 1
+            }),
+            'headers': {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => res.json()).then((data) => {
+            if(data.message === 'success'){
+                setJobPostings(data.data);
+            } else {
+                alert(data.data);
+            }
+        }).catch((err) => { alert(err) });
+    }, []);
 
     return (
         <Container className="bg-dark-subtle rounded p-4 my-5">
-            {postings.map((post, index) => (
+            {jobPostings.map((post, index) => (
                 <Card key={index} className="mb-3 p-3 bg-body-secondary">
                     <Row className="g-0">
                         <Col md={4}>
@@ -48,37 +54,17 @@ function CompanyPostings() {
                         </Col>
                         <Col md={8} className="d-flex justify-content-center align-items-center">
                             <Card.Body>
-                                <Card.Title className='fs-3 text' >{`Posting ID: ${post.id} | ${post.title}`}</Card.Title>
+                                <Card.Title className='fs-3 text' >{`Posting ID: ${post.id} | ${post.job_posting_title}`}</Card.Title>
                                 <br></br>
                                 <Card.Text>{`Salary: ${post.salary}`}</Card.Text>
-                                <Card.Text>{`Status: ${post.status}`}</Card.Text>
-                                <Card.Text>{`Position: ${post.position}`}</Card.Text>
-                                <Card.Text>{`Type: ${post.type}`}</Card.Text>
+                                <Card.Text>{`Status: ${post.job_posting_status}`}</Card.Text>
+                                <Card.Text>{`Position: ${post.job_posting_title}`}</Card.Text>
+                                <Card.Text>{`Type: ${post.job_posting_type}`}</Card.Text>
                             </Card.Body>
                         </Col>
                     </Row>
                 </Card>
             ))}
-            <Row xs={1} md={2} className="g-4">
-                {applicants.map((applicant, index) => (
-                    <Col key={index}>
-                        <Card className="h-100 p-3 bg-light">
-                            <Row className="g-0">
-                                <Col md={4}>
-                                    <Image src={applicant.picture} alt="Profile" className='border border-dark border-1' rounded fluid style={{ height: '200px', width: '100%', objectFit: 'cover' }} />
-                                </Col>
-                                <Col md={8}>
-                                    <Card.Body className='text-start'>
-                                        <Card.Title className='fw-medium text-decoration-underline'>{applicant.name}</Card.Title>
-                                        <Card.Text>{applicant.bio}</Card.Text>
-                                        <Card.Text>{`Professions: ${applicant.professions}`}</Card.Text>
-                                    </Card.Body>
-                                </Col>
-                            </Row>
-                        </Card>
-                    </Col>
-                ))}
-            </Row>
         </Container>
     );
 }
